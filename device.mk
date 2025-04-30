fbdd234f0a822ee89e6d5097f54513247101b6ef
Joey Huab <jhuab@edukasyon.ph>
2025-04-30 17:30:43 +0900

    venus: Move camera package name flag to select()

--- device.mk
+++ device.mk
@@ -9,6 +9,7 @@ $(call inherit-product, device/xiaomi/sm8350-common/common.mk)
 
 # Inherit from miuicamera-venus
 $(call inherit-product-if-exists, device/xiaomi/miuicamera-venus/device.mk)
+$(call soong_config_set,camera,package_name,com.android.camera)
 
 # Display Device Config
 PRODUCT_COPY_FILES += \
