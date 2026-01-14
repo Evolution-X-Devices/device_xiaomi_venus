568f86559f5ec4128569dfd5337bc3fe6abcca1f
Michael Bestas <mkbestas@lineageos.org>
2026-01-14 17:29:45 +0900

    venus: Patch some blobs to depend on libtinyxml2-v34.so
    libtinyxml2 10.1.0 update which landed in BP4A contains ABI
    incompatible changes which cause runtime crashes.
    
    Change-Id: Icc67da5f754b0bb30dce6192b1e520fdf1349ac5

--- extract-files.py
+++ extract-files.py
@@ -66,6 +66,10 @@ blob_fixups: blob_fixups_user_type = {
         .sig_replace('8D 0A 00 94', '1F 20 03 D5'),
     'vendor/lib64/vendor.xiaomi.hardware.cameraperf@1.0-impl.so': blob_fixup()
         .sig_replace('21 00 80 52 7C 00 00 94', '21 00 80 52 1F 20 03 D5'),
+    (
+        'vendor/lib/libaudiocloudctrl.so',
+    ): blob_fixup()
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
